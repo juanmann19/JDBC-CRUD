ba4ab05d7180aa492b16f6d168af99ce8940f0eb
Juan Garcia <Juanmanu2350@gmail.com>
2023-05-08 01:46:14 -0600

    Autocommit desactivado y control de excepciones en transacciones

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.alura.jdbc.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,12 +21,16 @@ public class ProductoController {
 	public int modificar(String nombre, String descripcion, Integer cantidad, Integer id) throws SQLException {
 		// TODO
 		Connection con = ConnectionFactory.recuperaConexion();
-		Statement statement = con.createStatement();
-		statement.execute("UPDATE PRODUCTO SET "
-	            + " NOMBRE = '" + nombre + "'"
-	            + ", DESCRIPCION = '" + descripcion + "'"
-	            + ", CANTIDAD = " + cantidad
-	            + " WHERE ID = " + id);
+		PreparedStatement statement = con.prepareStatement("UPDATE PRODUCTO SET "
+	            + " NOMBRE = ?" 
+	            + ", DESCRIPCION = ?" 
+	            + ", CANTIDAD = ?" 
+	            + " WHERE ID = ?");
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, Integer.valueOf(cantidad));
+		statement.setInt(4, Integer.valueOf(id));
+		statement.execute();
 		int updatecount = statement.getUpdateCount();
 		con.close();
 		return updatecount;
@@ -34,8 +39,9 @@ public class ProductoController {
 	public int eliminar(Integer id) throws SQLException {
 		// TODO
 		Connection con = ConnectionFactory.recuperaConexion(); 
-		Statement statement = con.createStatement();
-		statement.execute("DELETE FROM PRODUCTO WHERE id = " + id);
+		PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
+		statement.setInt(1, id);
+		statement.execute();
 		int updatecount = statement.getUpdateCount();
 		con.close();
 		return updatecount;
@@ -46,8 +52,8 @@ public class ProductoController {
 		// TODO
 		
 		Connection con = ConnectionFactory.recuperaConexion(); 
-		Statement statement = con.createStatement();
-		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		statement.execute();
 		ResultSet resultSet = statement.getResultSet();
 		List<Map<String, String>> resultado = new ArrayList<>(); 
 		while(resultSet.next()) {
@@ -68,20 +74,43 @@ public class ProductoController {
 
     public void guardar(Map<String, String> producto) throws SQLException {
 		// TODO
-		Connection con = ConnectionFactory.recuperaConexion();
-		Statement statement = con.createStatement();
-		statement.execute("INSERT INTO PRODUCTO (nombre, descripcion, cantidad)"
-				+ "VALUES ('" + producto.get("NOMBRE") + "', " 
-				+ "'" + producto.get("DESCRIPCION") + "', "
-				+ "'" + producto.get("CANTIDAD") + "')", Statement.RETURN_GENERATED_KEYS);
+		Connection con = ConnectionFactory.recuperaConexion();	
+		con.setAutoCommit(false);
+		String nombre = producto.get("NOMBRE"); 
+		String descripcion = producto.get("DESCRIPCION");
+		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
+		Integer maximoCantidad = 50;
+		
+		PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO "
+				+ "(nombre, descripcion, cantidad) "
+				+ "VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+		try {
+			do {
+				int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+				ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+				cantidad-=maximoCantidad;
+				
+			}while(cantidad > 0);
+			con.commit();
+		}catch(Exception e) {
+			con.rollback();
+		}
+		con.close();
+		statement.close();
+	}
+
+	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
+			throws SQLException {
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
+		statement.execute();
 		ResultSet result = statement.getGeneratedKeys();
 		while(result.next()) {
 			System.out.println(
 					String.format("El ID del valor insertado fue %d", result.getInt(1))
 					);
 		}
-
-
 	}
 
 }
