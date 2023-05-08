45786ddb0a5316b243749ff96ab276e986fe6eb2
Juan Garcia <Juanmanu2350@gmail.com>
2023-05-08 02:32:37 -0600

    Connection Pool configured

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,17 +1,27 @@
 package com.alura.jdbc.factory;
-
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+	private static EnvConfig config = new EnvConfig();
+	private static DataSource datasource;
+
 	
 	public static Connection recuperaConexion() throws SQLException {
-		EnvConfig config = new EnvConfig();
-		return DriverManager.getConnection(
-				config.url,
-				config.user,
-				config.pw);
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl(config.url);
+		pooledDataSource.setUser(config.user);
+		pooledDataSource.setPassword (config.pw);
+		datasource = pooledDataSource;
+		return datasource.getConnection();
+		//EnvConfig config = new EnvConfig();
+//		return DriverManager.getConnection(
+//				config.url,
+//				config.user,
+//				config.pw);
 	}
 
 }
--- src/main/java/com/alura/jdbc/factory/EnvConfig.java
+++ src/main/java/com/alura/jdbc/factory/EnvConfig.java
@@ -3,8 +3,8 @@ package com.alura.jdbc.factory;
 
 
 public class EnvConfig {
-		String url = System.getenv("DB_URL");
-		String user = System.getenv("DB_USER");
-		String pw = System.getenv("DB_PW");
+		public String url = System.getenv("DB_URL");
+		public String user = System.getenv("DB_USER");
+		public String pw = System.getenv("DB_PW");
 
 }
